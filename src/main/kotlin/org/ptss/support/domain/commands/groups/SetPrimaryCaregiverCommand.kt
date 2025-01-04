eaf923119498f967198cf283b7aeaf0cef4b4e64
SemPlaatsman <semplaatsman@gmail.com>
2025-01-04 21:53:47 +0100

    Removed unused command and handler

--- src/main/kotlin/org/ptss/support/domain/commands/groups/SetPrimaryCaregiverCommand.kt
+++
@@ -1,8 +0,0 @@
-package org.ptss.support.domain.commands.groups
-
-import java.util.UUID
-
-data class SetPrimaryCaregiverCommand(
-    val groupId: UUID,
-    val familyMemberId: UUID
-)
--- src/main/kotlin/org/ptss/support/infrastructure/handlers/commands/groups/SetPrimaryCaregiverCommandHandler.kt
+++
@@ -1,4 +0,0 @@
-package org.ptss.support.infrastructure.handlers.commands.groups
-
-class SetPrimaryCaregiverCommandHandler {
-}
