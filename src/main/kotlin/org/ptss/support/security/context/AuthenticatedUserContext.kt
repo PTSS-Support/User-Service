ec9b42314ec11d89fbf625b76a6d16999c6c8930
SemPlaatsman <semplaatsman@gmail.com>
2025-01-06 03:04:49 +0100

    Update AuthenticatedUserContext.kt

--- src/main/kotlin/org/ptss/support/security/context/AuthenticatedUserContext.kt
+++ src/main/kotlin/org/ptss/support/security/context/AuthenticatedUserContext.kt
@@ -3,13 +3,14 @@ package org.ptss.support.security.context
 import io.quarkus.security.UnauthorizedException
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import org.ptss.support.domain.constants.SecurityMessages.UNAUTHORIZED_ACCESS
 
 @ApplicationScoped
 class AuthenticatedUserContext @Inject constructor() {
     private val userContext = ThreadLocal<UserContext>()
 
     fun getCurrentUser(): UserContext = userContext.get()
-        ?: throw UnauthorizedException("No authenticated user context found")
+        ?: throw UnauthorizedException(UNAUTHORIZED_ACCESS)
 
     internal fun setCurrentUser(context: UserContext) {
         userContext.set(context)
