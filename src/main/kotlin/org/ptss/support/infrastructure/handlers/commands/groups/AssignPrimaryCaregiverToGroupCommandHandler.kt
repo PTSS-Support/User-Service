292390d11a8311fe9ea25ccedc0134198ce83c9c
SemPlaatsman <semplaatsman@gmail.com>
2025-01-17 22:13:51 +0100

    Adding group-related authentication service code

--- src/main/kotlin/org/ptss/support/infrastructure/handlers/commands/groups/AssignPrimaryCaregiverToGroupCommandHandler.kt
+++ src/main/kotlin/org/ptss/support/infrastructure/handlers/commands/groups/AssignPrimaryCaregiverToGroupCommandHandler.kt
@@ -8,21 +8,42 @@ import jakarta.ws.rs.NotFoundException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.ptss.support.domain.commands.groups.AssignPrimaryCaregiverToGroupCommand
+import org.ptss.support.domain.enums.Role
 import org.ptss.support.domain.interfaces.commands.groups.IAssignPrimaryCaregiverToGroupCommandHandler
 import org.ptss.support.domain.models.Group
+import org.ptss.support.infrastructure.external_services.auth.clients.IAuthenticationServiceClient
+import org.ptss.support.infrastructure.external_services.auth.dtos.requests.AuthUpdateRoleRequest
 import org.ptss.support.infrastructure.persistence.entities.GroupEntity
 import org.ptss.support.infrastructure.persistence.entities.GroupFamilyMemberEntity
 import org.ptss.support.infrastructure.persistence.entities.toModel
+import org.ptss.support.infrastructure.util.executeWithExceptionLoggingAsync
 
 @ApplicationScoped
-class AssignPrimaryCaregiverToGroupCommandHandler : IAssignPrimaryCaregiverToGroupCommandHandler {
-    override suspend fun handleAsync(command: AssignPrimaryCaregiverToGroupCommand): Group =
-        withContext(Dispatchers.IO) {
+class AssignPrimaryCaregiverToGroupCommandHandler(
+    private val authenticationServiceClient: IAuthenticationServiceClient
+) : IAssignPrimaryCaregiverToGroupCommandHandler {
+    override suspend fun handleAsync(command: AssignPrimaryCaregiverToGroupCommand): Group {
+        // First handle the database transaction
+        val (group, familyMember) = withContext(Dispatchers.IO) {
             handleTransaction(command)
         }
 
+        // Then update the role in the authentication service
+        executeWithExceptionLoggingAsync(
+            operation = {
+                authenticationServiceClient.updateRole(
+                    id = familyMember.user.keycloakId.toString(),
+                    request = AuthUpdateRoleRequest(role = Role.PRIMARY_CAREGIVER)
+                )
+            },
+            logMessage = "Failed to update user role to PRIMARY_CAREGIVER in authentication service"
+        )
+
+        return group
+    }
+
     @Transactional
-    fun handleTransaction(command: AssignPrimaryCaregiverToGroupCommand): Group {
+    fun handleTransaction(command: AssignPrimaryCaregiverToGroupCommand): Pair<Group, GroupFamilyMemberEntity> {
         Log.debug("Attempting to assign primary caregiver ${command.memberId} to group ${command.groupId}")
 
         val group = GroupEntity.findById(command.groupId)
@@ -45,6 +66,6 @@ class AssignPrimaryCaregiverToGroupCommandHandler : IAssignPrimaryCaregiverToGro
         group.persistAndFlush()
 
         Log.info("Successfully assigned primary caregiver ${command.memberId} to group ${command.groupId}")
-        return group.toModel()
+        return Pair(group.toModel(), familyMember)
     }
 }
--- src/main/kotlin/org/ptss/support/infrastructure/handlers/commands/groups/RemovePrimaryCaregiverFromGroupCommandHandler.kt
+++ src/main/kotlin/org/ptss/support/infrastructure/handlers/commands/groups/RemovePrimaryCaregiverFromGroupCommandHandler.kt
@@ -8,18 +8,38 @@ import jakarta.ws.rs.NotFoundException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.ptss.support.domain.commands.groups.RemovePrimaryCaregiverFromGroupCommand
+import org.ptss.support.domain.enums.Role
 import org.ptss.support.domain.interfaces.commands.groups.IRemovePrimaryCaregiverFromGroupCommandHandler
+import org.ptss.support.infrastructure.external_services.auth.clients.IAuthenticationServiceClient
+import org.ptss.support.infrastructure.external_services.auth.dtos.requests.AuthUpdateRoleRequest
 import org.ptss.support.infrastructure.persistence.entities.GroupEntity
+import org.ptss.support.infrastructure.util.executeWithExceptionLoggingAsync
+import java.util.UUID
 
 @ApplicationScoped
-class RemovePrimaryCaregiverFromGroupCommandHandler : IRemovePrimaryCaregiverFromGroupCommandHandler {
-    override suspend fun handleAsync(command: RemovePrimaryCaregiverFromGroupCommand) =
-        withContext(Dispatchers.IO) {
+class RemovePrimaryCaregiverFromGroupCommandHandler(
+    private val authenticationServiceClient: IAuthenticationServiceClient
+) : IRemovePrimaryCaregiverFromGroupCommandHandler {
+    override suspend fun handleAsync(command: RemovePrimaryCaregiverFromGroupCommand) {
+        // First handle the database transaction and get the user's keycloakId
+        val keycloakId = withContext(Dispatchers.IO) {
             handleTransaction(command)
         }
 
+        // Then update the role in the authentication service
+        executeWithExceptionLoggingAsync(
+            operation = {
+                authenticationServiceClient.updateRole(
+                    id = keycloakId.toString(),
+                    request = AuthUpdateRoleRequest(role = Role.FAMILY_MEMBER)
+                )
+            },
+            logMessage = "Failed to update user role to FAMILY_MEMBER in authentication service"
+        )
+    }
+
     @Transactional
-    fun handleTransaction(command: RemovePrimaryCaregiverFromGroupCommand) {
+    fun handleTransaction(command: RemovePrimaryCaregiverFromGroupCommand): UUID {
         Log.debug("Attempting to remove primary caregiver from group ${command.groupId}")
 
         val group = GroupEntity.findById(command.groupId)
@@ -31,10 +51,13 @@ class RemovePrimaryCaregiverFromGroupCommandHandler : IRemovePrimaryCaregiverFro
             throw BadRequestException("Group does not have a primary caregiver assigned")
         }
 
+        val keycloakId = group.primaryCaregiver!!.keycloakId
+
         // Remove primary caregiver role but keep as family member
         group.removeAsPrimaryCaregiver(group.primaryCaregiver!!)
         group.persistAndFlush()
 
         Log.info("Successfully removed primary caregiver from group ${command.groupId}")
+        return keycloakId
     }
 }
